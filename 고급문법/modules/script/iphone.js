5184f9cf8d0931ec63d197b14e8123d6e6b2d27c
seulkiu <alxsm90@naver.com>
2022-03-21 08:17:33 +0900

    슬롬과외

--- 고급문법/modules/script/iphone.js
+++ 고급문법/modules/script/iphone.js
@@ -1,2 +1,23 @@
-import { apple } from "./module";
-let iPhone = new apple("iphone", "150만원", "다용도");
+import { Electronics } from "./module";
+class Phone extends Electronics {
+    constructor(modelName, price, purpose, network){
+        super(modelName, price, purpose);
+        this.network = network;
+    }
+
+    setNetwork(network) {
+        this.network = network;
+    }
+
+    getNetwork(){
+        return this.network;
+    }
+
+    call(){
+        console.log("통화중입니다.");
+    }
+}
+
+let phone = new Phone("iphon12pro", "150만원", "다용도", "5g");
+phone.powerOn();
+phone.call();
